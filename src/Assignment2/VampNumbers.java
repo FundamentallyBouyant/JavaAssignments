d5d425d9de6957b24e9a055e30054920aa4cee5f
Jay Dadhania <jcdadhania@gmail.com>
2017-07-14 11:21:23 +0530

    Assignment 2

--- src/Assignment2/VampNumbers.java
+++ src/Assignment2/VampNumbers.java
@@ -1,52 +1,74 @@
 package Assignment2;
-//import java.lang.*;
+
 import java.util.Arrays;
 
 
-/**
- * Created by Jay Dadhania on 12/7/17.
+
+
+ /**
+ * Generates Vampire numbers
+ * @author Jay Dadhania
  */
-
-
 public class VampNumbers {
+    /**
+     * Prints fangs for a Number if it is a Vampire number
+     * @param n Number to get fangs for
+     * @param startRange Start range (i.e. 1000,1000000 etc.)
+     * @return <code>true</code> if the number is a vampire number;
+     *                  <code>false</code> otherwise.
+     */
+    public Boolean getFangs(int n,int startRange){
+        for(int j= (int) Math.sqrt(startRange/10);j<= Math.sqrt(n);j++){
+            int fang = n/j;
+            if(fang*j == n){
+                if(checkIfVampFangs(fang,j)){
+                    return true;
+                };
+            }
+        }
+        return false;
+    }
 
-    static Boolean checkIfVampFangs(int fang1, int fang2){
+    /**
+     * Check if two numbers can form a Vampire number if multiplied. Prints the Vampire number and fangs if they form a Vampire Number
+     * @param fang1 First number
+     * @param fang2 Second number
+     * @return <code>true</code> if the numbers form a vampire number;
+     *                  <code>false</code> otherwise.
+     */
+    public Boolean checkIfVampFangs(int fang1, int fang2){
         String fang1String = Integer.toString(fang1);
         String fang2String = Integer.toString(fang2);
         if(fang1String.length() == fang2String.length() && !(fang1%10==0 && fang2%10 == 0)){
             String vampString = Integer.toString(fang1*fang2);
             String teethString = fang1String + fang2String;
-
             char[] vampArray = vampString.toCharArray();
             char[] teethArray = teethString.toCharArray();
-
             Arrays.sort(vampArray);
             Arrays.sort(teethArray);
-
-            if(Arrays.equals(vampArray,teethArray)) return true;
+            if(Arrays.equals(vampArray,teethArray)){
+                System.out.println(fang1*fang2 + " = (" + fang1 + " * " + fang2 +")");
+                return true;
+            }
         }
-
         return false;
-
     }
 
-    public static void main(String[] args) {
-        System.out.println(VampNumbers.checkIfVampFangs(1620,8073));
-        int i = 10;
-        int count = 0;
-        while(count<10){
-            for(int j= i; j< i*10;j++) {
-                for (int k = j; k < i*10 ; k++) {
-//                    System.out.println("aaaa");
-//                    System.out.println(count+ " " +(j*k));
-                    if (checkIfVampFangs(j, k)) {
-                        count++;
-                        System.out.println( count+": "+j + " " + k + " = " + (j*k));
-                    }
+    public static void getVampireNumbers(int n){
+        VampNumbers vamp = new VampNumbers();
+        int startRange = 1000;
+        int count = 1;
+        while((startRange < (Integer.MAX_VALUE / 1000))) {
+            for (int i = startRange; i < startRange * 10; i++) {
+                if (count > n) return;
+                if (vamp.getFangs(i, startRange)) {
+                    count++;
                 }
-
             }
-            i = i*10;
+            startRange *= 100;
         }
+    }
+    public static void main(String[] args) {
+        getVampireNumbers(100);
     }
 }
